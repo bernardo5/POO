0d4dff3e018fd1f2518b110adad26ea3098f0dcf
bernardo5 <bdnabaisgomes@yahoo.com>
2016-04-19 22:27:06 +0100

    deck e cartas ok

---
+++ proj/blackjack/src/blackjackG21/Card.java
@@ -0,0 +1,18 @@
+package blackjackG21;
+
+public class Card {
+	private String name;	
+	private char suit;
+	
+	public Card(String name, char suit){
+		this.name=name;
+		this.suit=suit;
+	}
+
+	@Override
+	public String toString() {
+		return "Card=(" + name + suit + ")";
+	}
+	
+	
+}
