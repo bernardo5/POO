eb32c829c6b3cf18f6c3d576c2aa1c6c9c376437
bernardo5 <bdnabaisgomes@yahoo.com>
2016-05-10 14:41:49 +0100

    sem out of bounds

--- proj/Basic.java
+++ proj/Basic.java
@@ -3,10 +3,22 @@ package blackjack;
 public class Basic implements ActionStrategy{
 	@Override
 	public String advice(Hand player_hand,Card dealer_card) {
-		if (player_hand.getSizeofCards() == 2 && player_hand.getCards().getFirst().getRank()==player_hand.getCards().getLast().getRank()) 
-		return pair_advice[player_hand.getCards().getFirst().getValue()-2][dealer_card.getValue()-2];
-		else if (player_hand.numberAces()>0) return soft_advice[player_hand.getPoints()-13][dealer_card.getValue()-2];
-		else return hard_advice[player_hand.getPoints()-5][dealer_card.getValue()-2];
+		if (player_hand.getSizeofCards() == 2 && 
+				player_hand.getCards().getFirst().getRank()==player_hand.getCards().getLast().getRank()){
+			return pair_advice[player_hand.getCards().getFirst().getValue()-2][dealer_card.getValue()-2];
+		}
+		else if (player_hand.numberAces()>0){
+			String ret=new String();
+			try{
+				ret=soft_advice[player_hand.getPoints()-13][dealer_card.getValue()-2];
+			}catch(ArrayIndexOutOfBoundsException e){
+				ret=hard_advice[player_hand.getPoints()-5][dealer_card.getValue()-2];
+			}
+			return ret;
+		}
+		else{
+				return hard_advice[player_hand.getPoints()-5][dealer_card.getValue()-2];		
+		}
 	}
 
 }
